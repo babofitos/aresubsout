66a5f24567e040cf854c227279b29548dae7918b
babofitos <tehchoi@gmail.com>
2013-06-01 00:02:18 -0400

    filters update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
 
 if (localStorage.animes) {
-  $.ajax({
-    type: 'POST'
-  , url: window.location.href
-  , data: {data: localStorage.animes}
-  , error: function() {
-    $('#notification').html('Error retrieving data')
-  }
-  , success: function(data) {
-      displayData(data)
-    }
-  })
+  buildFilters()
+  fetch()
 } else {
   $('#notification').html('It looks like you have no anime added')
 }
@@ -22,24 +13,79 @@ $('#add-show').on('click', function(e) {
   data.push($('#add-show-text').val().toLowerCase())
   localStorage.animes = JSON.stringify(data)
   $('#add-show-text').val('')
+  updateFilters()
+})
+
+$('#filters').on('click', function(e) {
+  if ($(e.target).is('li')) {
+    var target = $(e.target)
+    removeFromLS(target.html(), target.index())
+  }
 })
 
 function displayData(data) {
-  var len = data.results.length
+  var len = data.length
   for (var i=0;i<len;i++) {
     $('#list').append(
       $('<li/>', {
         class: 'item'
-      , html: makeLink(data.results[i])
+      , html: makeLink(data[i])
       })
     )
   }
 }
+
+function buildFilters() {
+  var list = JSON.parse(localStorage.animes)
+    , len = list.length
+
+  for (var i=0;i<len;i++) {
+    $('#filters').append(
+      $('<li/>', {
+        class: 'filters'
+      , text: list[i]
+      })
+    )
+  }
+}
+
+function updateFilters() {
+  //rebuild filters
+  $('#filters').html('')
+  buildFilters()
+}
+
+function fetch() {
+  $.ajax({
+    type: 'POST'
+  , url: window.location.href
+  , data: {data: localStorage.animes}
+  , error: function() {
+    $('#notification').html('Error retrieving data')
+  }
+  , success: function(data) {
+      displayData(data.results)
+      showPubDate(data.date)
+    }
+  })
+}
+
 function makeLink(data) {
   return $('<a/>', {
     class: 'list-link'
   , href: data.link
   , text: data.title
   })
+}
+
+function showPubDate(date) {
+  $('#date').html(date)
+}
+
+function removeFromLS(filter, index) {
+  var animes = localStorage.animes ? JSON.parse(localStorage.animes) : []
+  animes.splice(index, 1)
+  localStorage.animes = JSON.stringify(animes)
+  updateFilters()
 }
 })
