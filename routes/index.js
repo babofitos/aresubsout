57f63eef381980ac8aace7d35aebb865484e56f9
babofitos <tehchoi@gmail.com>
2013-06-05 18:23:59 -0400

    abstract db out of parse and refactor

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
  * GET home page.
  */
 var parse = require('../lib/parse.js')
+  , db = require('../lib/db.js')
 
 exports.index = function(req, res) {
   res.render('index', { 
@@ -12,19 +13,13 @@ exports.index = function(req, res) {
 
 exports.show = function(req, res, next) {
   //list of animes
-  var animes = JSON.parse(req.body.data)
-  if (animes.length === 0) {
+  var filter = JSON.parse(req.body.data)
+  if (filter.length === 0) {
     res.send(204)
     return
   }
-  
-  parse.list(animes, function(err, results) {
-    if (err) {
-      console.log('parsing error', err)
-      next(err)
-    }
-    else {
-      res.send(200, {results: results})
-    }
+  db.findArticle(filter, function(err, results) {
+    if (err) next(err)
+    else res.send(200, {results: results})
   })
 }
