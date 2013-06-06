86491a18f04049535ecc0e8044fcb79e8ef8d457
babofitos <tehchoi@gmail.com>
2013-06-06 18:28:51 -0400

    limit all types

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.configure(function(){
   app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(type('application/json', express.limit('100kb')))
+  app.use(express.limit('100kb'))
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
